ca19528c3017e565890f38d64b3f4e72026271e3
pmancevski <pmancevskiwork@gmail.com>
2023-08-05 00:46:42 +0200

    test

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -8,5 +8,7 @@ public class ConferenceDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConferenceDemoApplication.class, args);
+
+        //test
     }
 }
