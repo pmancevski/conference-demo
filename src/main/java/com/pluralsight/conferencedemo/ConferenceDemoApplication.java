37e22156768bd945d80f34053afb1d5ef2fdccc4
pmancevski <pmancevskiwork@gmail.com>
2023-08-05 22:43:21 +0200

    Align commit and push

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -8,7 +8,5 @@ public class ConferenceDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConferenceDemoApplication.class, args);
-
-        //test
     }
 }
